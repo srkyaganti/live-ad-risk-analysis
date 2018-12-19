54b4dfb6d2a751b4124cc65ba80fac3b96a6eb9b
Gowtham Kurapati <gkurapat@amazon.com>
2018-12-19 20:05:33 +0530

    mysql connection object

---
+++ mysql/Connection.js
@@ -0,0 +1,37 @@
+
+
+
+const mysql      = require('mysql');
+const pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'live-ad-risk-db.cxtxk2crdbbz.us-east-1.rds.amazonaws.com',
+    user            : 'gkurapat',
+    password        : 'syyagant',
+    database        : 'booker'
+  });
+  module.exports = twitterClient;
+  /*
+  pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+
+*/
+/*
+var connection = mysql.createConnection({
+  host     : 'live-ad-risk-db.cxtxk2crdbbz.us-east-1.rds.amazonaws.com',
+  user     : 'gkurapat',
+  password : 'syyagant'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+*/
+
+console.log("hello gowtham");
